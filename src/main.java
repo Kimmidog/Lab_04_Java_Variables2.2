1b971fb22003915da02fc1c71c1b930c0019926c
Kimmidog <Kimmidog4@gmail.com>
2024-09-24 14:48:59 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,57 @@
+public class main {
+    public static void main(String[] args){
+        int intOperandA = 1;
+        int intOperandB = 2;
+        int intSum = 3;
+        int intProduct = 2;
+        int intDifference = -1;
+        int intQuotient = 0;
+        int intModulo = 1;
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 4.50;
+        double doubleSum = 8.0;
+        double doubleProduct = 15.75;
+        double doubleDifference = -1.0;
+        double doubleQuotient = .777;
+
+        intSum = intOperandA + intOperandB; //Assignment that uses the arithmetic operator +
+        intProduct = intOperandA * intOperandB; //Assignment that uses the arithmetic operator *
+        intDifference = intOperandA - intOperandB; //Assignment that uses the arithmetic operator -
+        intQuotient = intOperandA / intOperandB; //Assignment that uses the arithmetic operator /
+        intModulo = intOperandA % intOperandB; //Assignment that uses the arithmetic operator %
+
+        doubleSum = doubleOperandA + doubleOperandB; //Assignment that uses the arithmetic operator +
+        doubleProduct = doubleOperandA * doubleOperandB; //Assignment that uses the arithmetic operator *
+        doubleDifference = doubleOperandA - doubleOperandB; //Assignment that uses the arithmetic operator -
+        doubleQuotient = doubleOperandA / doubleOperandB; //Assignment that uses the arithmetic operator /
+
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum); //output statement
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct); //output statement
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference); //output statement
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient); //output statement
+        System.out.println("The remainder using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo); //output statement
+             System.out.println( );
+        System.out.println("The sum using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum); //output statement
+        System.out.println("The product using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct); //output statement
+        System.out.println("The difference using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference); //output statement
+        System.out.println("The quotient using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient); //output statement
+
+        System.out.println( );
+
+        int childrenInFamily = 1;
+        boolean isItRaining = false;
+        double galOfGas = 2.91;
+        int favNum = 4;
+        double shoeSize = 12.5;
+        int birthMonth = 10;
+        String myFullName = "Winter Barbee";
+        System.out.println("The number of children in my family is " + childrenInFamily);
+        System.out.println("The answer to is it raining is " + isItRaining);
+        System.out.println("The cost for a gallon of gas is " + galOfGas);
+        System.out.println("My favorite number is " + favNum);
+        System.out.println("My shoe size is " + shoeSize);
+        System.out.println("My birth month is " + birthMonth);
+
+    }
+
+}
